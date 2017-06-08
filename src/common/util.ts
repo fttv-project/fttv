aea8eb5403b99ad5a93c1385dda342eea132d528
Oden <ops991923@gmail.com>
2017-06-08 14:50:08 -0700

    Remove extra space

--- src/common/util.ts
+++ src/common/util.ts
@@ -6,7 +6,7 @@ export const subtleCrypto = crypto.subtle || (crypto as any).webkitSubtle;
 const alphanumericCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 export const generateRandomString = (length: number): string => {
 	let result = "";
-	const randomValues =  new Uint32Array(length);
+	const randomValues = new Uint32Array(length);
 	crypto.getRandomValues(randomValues);
 	for (const value of randomValues) {
 		result += alphanumericCharacters[value % alphanumericCharacters.length];
