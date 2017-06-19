ad1279a276a80de80039d1b467314d13665bdaa1
Gustorn <gustorn@gmail.com>
2017-06-19 12:49:10 +0200

    Add route preloading

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -5,6 +5,7 @@ import { Route } from "react-router";
 import { connect } from "react-redux";
 
 import Loadable from "common/loadable";
+import { idleCallback } from "common/util";
 import { State } from "data";
 import { Theme } from "data/settings";
 import Home from "pages/home";
@@ -16,25 +17,46 @@ const Auth = Loadable(() => System.import("pages/auth"));
 const Settings = Loadable(() => System.import("pages/settings"));
 const Directory = Loadable(() => System.import("pages/directory"));
 
-const App = ({ theme }: Props) => (
-	<CustomProperties global properties={theme.properties}>
-		<main className={style.container}>
-			<Helmet
-				defaultTitle={manifest.short_name}
-				titleTemplate={`%s - ${manifest.short_name}`}
-			/>
+const preloadedRoutes = [
+	Directory,
+	Auth,
+	Settings
+];
 
-			<NavigationBar />
-			<div className={style.page}>
-				<Route exact path="/" component={Home} />
-				<Route exact path="/auth" component={Auth} />
-				<Route exact path="/settings" component={Settings} />
-				<Route exact path="/directory" component={Directory} />
-				<Route exact path="/directory/following" component={Directory} />
-			</div>
-		</main>
-	</CustomProperties>
-);
+class App extends React.Component<Props, {}> {
+	componentDidMount() {
+		this.preloadRoutes();
+	}
+
+	render() {
+		const { theme } = this.props;
+		return (
+			<CustomProperties global properties={theme.properties}>
+				<main className={style.container}>
+					<Helmet
+						defaultTitle={manifest.short_name}
+						titleTemplate={`%s - ${manifest.short_name}`}
+					/>
+
+					<NavigationBar />
+					<div className={style.page}>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/auth" component={Auth} />
+						<Route exact path="/settings" component={Settings} />
+						<Route exact path="/directory" component={Directory} />
+						<Route exact path="/directory/following" component={Directory} />
+					</div>
+				</main>
+			</CustomProperties>
+		);
+	}
+
+	private preloadRoutes = () => {
+		for (const route of preloadedRoutes) {
+			idleCallback(route.preload);
+		}
+	}
+}
 
 type Props = StoreProps;
 
